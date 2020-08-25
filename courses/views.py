e19eb5047c33ae5a7cb0dac39e9ede81c9262079
Gleb Uvarov <ugs1996@mail.ru>
2020-08-25 14:32:20 +0300

    добавлен декоратор auth_and_parse

--- courses/views.py
+++ courses/views.py
@@ -4,22 +4,25 @@ from rest_framework.decorators import api_view
 
 from courses import models
 
+from authentication.decorators import auth_and_parse
 
 @api_view(['POST'])
 @csrf_exempt
-def get_document(request):
-    if 'doc_id' in request.POST:
-        response = models.Document.get_one(request.POST['doc_id'])
-        return JsonResponse(response)
-    return JsonResponse({'error': 'doc_id is undefined'})
+@auth_and_parse
+def get_document(system, data):
+    if 'doc_id' in data:
+        response, status = models.Document.get_one(data['doc_id'])
+        return response, status
+    return {'error': 'doc_id is undefined'}, 400
 
 
 @api_view(['POST'])
 @csrf_exempt
-def get_folders(request):
-    if 'folder_id' in request.POST:
-        response = { 'documents': models.Document.get_list(request.POST['folder_id']) }
-        return JsonResponse(response)
+@auth_and_parse
+def get_folders(system, data):
+    if 'folder_id' in data:
+        response = { 'documents': models.Document.get_list(data['folder_id']) }
+        return response, 200
     else:
-        responce = { 'folders': models.Folder.get_list() }
-        return JsonResponse(responce)
+        response = { 'folders': models.Folder.get_list() }
+        return response, 200
