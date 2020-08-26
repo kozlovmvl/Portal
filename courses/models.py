17f46af214225ead34b57b8f7b4757a2803cd45b
Gleb Uvarov <ugs1996@mail.ru>
2020-08-26 11:31:34 +0300

    добавлены модели LikeDocument и ViewDocument. Добавлены новые API к ним

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.db.models import OuterRef, Count, Exists
 
+from authentication.models import CustomUser
 
 class Folder(models.Model):
 
@@ -8,7 +10,7 @@ class Folder(models.Model):
         verbose_name_plural='папки'
 
     title = models.CharField('папка', max_length=255)
-    preview = models.ImageField('превью')
+    preview = models.ImageField('превью', upload_to='upload/preview/%Y/%m/%d/')
     is_visible = models.BooleanField('видимость', default=False)
 
     def __str__(self):
@@ -27,9 +29,10 @@ class Document(models.Model):
 
     title = models.CharField('название документа', max_length=255)
     description = models.TextField('описание')
-    preview = models.ImageField('превью')
+    preview = models.ImageField('превью', upload_to='upload/preview/%Y/%m/%d/')
     created = models.DateTimeField('дата создания', auto_now_add=True)
     is_visible = models.BooleanField('видимость')
+    views = models.PositiveIntegerField('просмотры', default=0)
     folder = models.ForeignKey(
         'Folder', verbose_name='папка', on_delete=models.CASCADE)
 
@@ -37,9 +40,16 @@ class Document(models.Model):
         return self.title
 
     @classmethod
-    def get_list(cls, folder_id):
-        return list(cls.objects.filter(folder=folder_id).values(
-            'id', 'title', 'description', 'created', 'preview'))
+    def get_list(cls, user, folder_id):
+        subquery = LikeDocument.objects.filter(user=user, doc=OuterRef('id'))
+        list_obj = list(cls.objects.filter(folder=folder_id).values(
+            'id', 'title', 'description', 'created', 'preview', 'views'
+            ).annotate(
+                likes=Count('likes'),
+            ).annotate(
+                is_liked=Exists(subquery))
+            )
+        return list_obj
             
 
     @classmethod
@@ -52,8 +62,20 @@ class Document(models.Model):
             return {'error': 'doc_id is invalid'}, 400
         except cls.DoesNotExist:
             return {'error': 'id not find'}, 400
+    
+    @classmethod
+    def create_or_update_views(cls, user, doc_id):
+        doc = cls.objects.get(id=doc_id)
+        view_doc, created = ViewDocument.objects.get_or_create(user=user, doc=doc)
+        if created:
+            doc.views += 1
+            doc.save(update_fields=['views'])
+        else:
+            view_doc.save(update_fields=['date'])
 
+        return created
 
+            
 class Element(models.Model):
     
     CHOICES = [
@@ -68,11 +90,11 @@ class Element(models.Model):
 
     doc = models.ForeignKey(
         'Document', verbose_name='документ',
-        related_name='document', on_delete=models.CASCADE)
+        related_name='elements', on_delete=models.CASCADE)
     type = models.CharField('тип', max_length=5, choices=CHOICES)
     text = models.TextField('текст')
     order = models.PositiveIntegerField('порядок')
-    file = models.FileField('файл', upload_to='upload/%Y/%m/%d/')
+    file = models.FileField('файл', upload_to='upload/files/%Y/%m/%d/')
 
 
     @classmethod
@@ -82,3 +104,22 @@ class Element(models.Model):
 
     def __str__(self):
         return self.type
+
+
+class ViewDocument(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    doc = models.ForeignKey('Document', related_name="view", verbose_name="документ", on_delete=models.CASCADE)
+    date = models.DateTimeField('время просмотра', auto_now=True)
+
+    def __str__(self):
+        return "{}'ом просмотрен документ {}".format(self.user, self.doc)
+
+class LikeDocument(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    doc = models.ForeignKey('Document', related_name="likes", verbose_name="документ", on_delete=models.CASCADE)
+
+    @classmethod
+    def set_like(cls, user, doc_id):
+        doc = Document.objects.get(id=doc_id)
+        like_doc, created = cls.objects.get_or_create(user=user, doc=doc)
+        return created
--- courses/views.py
+++ courses/views.py
@@ -10,6 +10,7 @@ from courses import models
 @auth_and_parse
 def get_document(system, data):
     if 'doc_id' in data:
+        models.Document.create_or_update_views(system['__user'], data['doc_id'])
         response, status = models.Document.get_one(data['doc_id'])
         return response, status
     return {'error': 'doc_id is undefined'}, 400
@@ -18,10 +19,21 @@ def get_document(system, data):
 @api_view(['POST'])
 @csrf_exempt
 @auth_and_parse
+def like_document(system, data):
+    if 'doc_id' in data:
+        status = models.LikeDocument.set_like(system['__user'], data['doc_id'])
+        return {'status': status}, 200
+    return {'error': 'doc_id is undefined'}, 400
+
+
+@api_view(['POST'])
+@csrf_exempt
+@auth_and_parse
 def get_folders(system, data):
     if 'folder_id' in data:
-        response = { 'documents': models.Document.get_list(data['folder_id']) }
+        response = { 'documents': models.Document.get_list(system['__user'], data['folder_id']) }
         return response, 200
     else:
         response = { 'folders': models.Folder.get_list() }
         return response, 200
+
