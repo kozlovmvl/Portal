095c26452ee65b8e95ccd009e06258c4d63a6a5d
Gleb Uvarov <ugs1996@mail.ru>
2020-08-26 16:46:50 +0300

    переведены названия моделей и поля

--- tests/models.py
+++ tests/models.py
@@ -3,13 +3,13 @@ from django.db import models
 class Test(models.Model):
 
     class Meta:
-        verbose_name = 'test'
-        verbose_name_plural = 'tests'
+        verbose_name = 'тест'
+        verbose_name_plural = 'тесты'
     
-    title = models.CharField('title', max_length=255)
-    preview = models.ImageField('preview', upload_to='upload/tests/preview/%Y/%m/%d/')
-    is_visible = models.BooleanField('is_visible')
-    min_result = models.DecimalField('decimal', max_digits=100, decimal_places=1)
+    title = models.CharField('название теста', max_length=255)
+    preview = models.ImageField('превью', upload_to='upload/tests/preview/%Y/%m/%d/')
+    is_visible = models.BooleanField('видимость')
+    min_result = models.DecimalField('мин. результат', max_digits=100, decimal_places=1)
 
     def __str__(self):
         return self.title
@@ -23,12 +23,12 @@ class Question(models.Model):
     ]
 
     class Meta:
-        verbose_name = 'question'
-        verbose_name_plural = 'questions'
+        verbose_name = 'вопрос'
+        verbose_name_plural = 'вопросы'
 
-    type = models.CharField('type', max_length=8, choices=CHOICES)
-    text = models.TextField('text')
-    order = models.PositiveIntegerField('order')
+    type = models.CharField('тип поля', max_length=8, choices=CHOICES)
+    text = models.TextField('текст')
+    order = models.PositiveIntegerField('порядок')
 
     def __str__(self):
         if len(self.text) > 70:
@@ -39,13 +39,13 @@ class Question(models.Model):
 class Option(models.Model):
 
     class Meta:
-        verbose_name = 'option'
-        verbose_name_plural = 'options'
+        verbose_name = 'опция'
+        verbose_name_plural = 'опции'
 
-    text = models.TextField('text')
-    is_right = models.BooleanField('is_right')
-    order = models.PositiveIntegerField('order')
-    question = models.ForeignKey('Question', related_name="options", on_delete=models.CASCADE)
+    text = models.TextField('текст')
+    is_right = models.BooleanField('верность ответа')
+    order = models.PositiveIntegerField('порядок')
+    question = models.ForeignKey('Question', verbose_name="вопрос", related_name="options", on_delete=models.CASCADE)
 
     def __str__(self):
         if len(self.text) > 70:
