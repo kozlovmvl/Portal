139ee8b22a1852f81b70bb917adfac0bb2132a50
kozlovmv <kozlov.mvl@yandex.ru>
2020-09-04 10:19:27 +0300

    Добавлен класс для формирования результатов тестов.

---
+++ statistics/utils.py
@@ -0,0 +1,64 @@
+import attr
+from django.db.models import Exists, F, Value, OuterRef, CharField
+from django.db.models.functions import Concat
+
+from authentication.models import CustomUser
+from tests.models import Attempt, Test
+
+
+@attr.s(auto_attribs=True)
+class ResultsFactory:
+    user_id = attr.ib(
+        default=None,
+        validator=attr.validators.optional(attr.validators.instance_of(int))
+    )
+    test_id = attr.ib(
+        default=None,
+        validator=attr.validators.optional(attr.validators.instance_of(int))
+    )
+
+    def __get_for_user(self):
+        attempts_qs = Attempt.objects.filter(
+            user_id=self.user_id,
+            test_id=OuterRef('id'),
+            result__gte=OuterRef('min_result')
+        )
+        tests_qs = Test.objects.filter(is_visible=True) \
+            .order_by('title').values('title', is_success=Exists(attempts_qs))
+        return list(tests_qs)
+
+    def __get_for_test(self):
+        attempts_qs = Attempt.objects.filter(
+            user_id=OuterRef('id'),
+            test_id=self.test_id,
+            result__gte=OuterRef('min_result')
+        )
+        users_qs = CustomUser.objects.filter(role='user').values(
+            user=Concat(
+                F('last_name'),
+                Value(' ', CharField()),
+                F('first_name')
+            ),
+            is_success=Exists(attempts_qs)
+        ).order_by('user')
+        return list(users_qs)
+
+    def __get_for_user_by_test(self):
+        attempts_qs = Attempt.objects.filter(
+            user_id=self.user_id,
+            test_id=self.test_id,
+            result__gte=OuterRef('min_result')
+        )
+        test = Test.objects.values(
+            'title',
+            is_success=Exists(attempts_qs)
+        ).get(id=self.test_id)
+        return dict(test)
+
+    @property
+    def data(self):
+        if self.user_id and self.test_id:
+            return self.__get_for_user_by_test()
+        if self.test_id:
+            return self.__get_for_test()
+        return self.__get_for_user()
--- statistics/views.py
+++ statistics/views.py
@@ -1,23 +1,14 @@
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 
 from authentication.decorators import auth_and_parse
-from rest_framework.decorators import api_view
-from tests import models
+from .utils import ResultsFactory
 
 
 @api_view(['POST'])
 @csrf_exempt
 @auth_and_parse
 def result_statistics(system, data):
-    list_obj = None
-    if ('test_id' in data) and ('user_id' in data):
-        list_obj = models.Test.get_statistics_user_test(data['user_id'], data['test_id'])
-    elif 'test_id' in data:
-        list_obj = models.Test.get_list_statistics_for_test(data['test_id'])
-    elif 'user_id' in data:
-        list_obj = models.Test.get_list_statistics(users=[data['user_id']])
-    else:
-        list_obj = models.Test.get_list_statistics()
-    if list_obj is not None:
-        return {'results': list_obj}, 200
-    return {'error': 'test_id is not exist'}, 200
+    results = ResultsFactory(**data)
+    return results.data, 200
+
